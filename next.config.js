a6d8380d3ef34b0cd9bc7e45d2c33db5915f20fe
marwane999 <marwane999@users.noreply.github.com>
2026-06-20 13:32:47 +0100

    Disable Next.js image optimization for reliable deployment

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'coachconsulting.ma',
-        pathname: '/images/**',
-      },
-    ],
+    unoptimized: true,
   },
 }
 module.exports = nextConfig
